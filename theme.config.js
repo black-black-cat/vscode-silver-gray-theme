2687dd9904ce5f174f9ac1a38da31bfe8d23b4e2
black-black-cat <applyk99@163.com>
2021-02-20 17:35:27 +0800

    Use Noctis Token Colors Rules

---
+++ theme.config.js
@@ -0,0 +1,43 @@
+module.exports = [{
+  name: 'Silver Gray',
+  inputFile: 'src/theme.json',
+  outputDir: 'output',
+  colors: {
+    comment: '#858585',
+    text: '#222',
+    keyword: '#80a',
+    variable: '#222',
+    annotation: '#80a',
+    constant: '#222',
+    tag: '#80a',
+    string: '#007fbf',
+    stringInTemplate: '#D000D0',
+    number: '#750',
+    function: '#00e',
+    support: '#080',
+    misc: '#080',
+    invalid: '#a00',
+    objectProperty: '#555'
+  }
+}, {
+  name: 'Primer',
+  inputFile: 'src/primer.json',
+  outputDir: 'output',
+  colors: {
+    comment: '#959da5',
+    text: '#24292e',
+    keyword: '#d03592',
+    variable: '#24292e',
+    annotation: '#d03592',
+    constant: '#22863a',
+    tag: '#d03592',
+    string: '#d15704',
+    stringInTemplate: '#d15704',
+    number: '#6f42c1',
+    function: '#005cc5',
+    support: '#22863a',
+    misc: '#22863a',
+    invalid: '#d73a49',
+    objectProperty: '#24292e'
+  }
+}]
